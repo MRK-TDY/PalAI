44f9b187e6f93b6eab11797d7357179174d371e1
Manuel Guimarães <manuel.guimaraes@mrkite.io>
2024-02-02 16:02:21 +0000

    Python Server is now in communication with C++ Project

--- Algorithm/dataextractor.py
+++ Algorithm/dataextractor.py
@@ -1,4 +1,11 @@
 
+default_response = "B:Cube|X=2680.000 Y=-1880.000 Z=110.000|3\n" \
+                   "B:Cube|X=2880.000 Y=-2280.000 Z=110.000|3\n" \
+                   "B:Cube|X=2880.000 Y=-2081.000 Z=110.000|3\n" \
+                   "B:Cube|X=2880.000 Y=-1880.000 Z=110.000|3\n" \
+                   "B:Cube|X=3080.000 Y=-1880.000 Z=110.000|3\n"
+
 
 def extract_data(data):
     print("Extracting data: \n" + str(data))
+    return default_response
--- Server/server.py
+++ Server/server.py
@@ -25,16 +25,14 @@ class ServerHandler(BaseHTTPRequestHandler):
     # Handle GET requests
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        print(str(self.headers))
-
         content_length = int(self.headers['Content-Length'])   # <--- Gets the size of data
         type = self.headers.get_content_type()
         self._set_response()
         if (len(extractedData) > 0):
             post_data = self.rfile.read(content_length)  # <--- Gets the data itself
-            data = post_data.decode('utf-8')
-            #self.wfile.write(bytes(extractedData[self.client_address[0]], encoding="utf-8"))
-            self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+            self.wfile.write(bytes(extractedData[self.client_address[0]], encoding="utf-8"))
+            print("GET -- Sending data: " + str(extractedData[self.client_address[0]]))
+            #self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
         else:
             self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
@@ -48,11 +46,10 @@ class ServerHandler(BaseHTTPRequestHandler):
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
         if (len(data) > -1):
 
-            ## Saving Data to an Array
-            extractedData[self.client_address[0]] = data
-
             ##Extract Data
-            dataextractor.extract_data(data)
+            result = dataextractor.extract_data(data)
+            ## Saving Data to an Array
+            extractedData[self.client_address[0]] = result
 
 Handler = ServerHandler
 
