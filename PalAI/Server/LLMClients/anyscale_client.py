11c1b1f6f3b05a620e0f91ca2a2023e55bd1bd40
Manuel Guimarães <manuel.guimaraes@mrkite.io>
2024-03-20 11:47:24 +0000

    Updated Anyscale client to remove outdated prompts. Continue to add output_evaluators.

--- PalAI/Server/LLMClients/anyscale_client.py
+++ PalAI/Server/LLMClients/anyscale_client.py
@@ -21,25 +21,6 @@ class AnyscaleClient(LLMClient):
             api_key=self.api_key
         )
 
-
-        self.system_prompt = "You are a virtual architect for a game called TODAY. Your job is to respond to user's requests and plan out in detail the building they want to construct.\n" \
-                             "Buildings are created layer by layer, and can be 3 layers tall, labelled from 0 to 2. Buildings are made up of cubes." \
-                             "  The first layer, layer 0 is directly above the ground. The higher the number of the layer the higher it is placed." \
-                             "  Buildings in today should be solid, which means that if the player requests a square house, instead of making it just the walls, you should fill the insides with cubes as well." \
-                             "  In other words, if a player wants a square house you should build a complete 5 by 5 square that is 3 layers tall." \
-                             "  In addition, buildings should have some structural integrity, so blocks can't be floating or unsupported." \
-                             "  Structures should also maintain some realism, meaning that you shouldn't build structures that are impossible to support." \
-                             "  Use natural language when describing the plans for the buildings. Use expressions like create a row of blocks at..., etc..."
-
-        self.system_prompt2 = "ARCHITECT: In order to build a house that widens at the top we must place 3 square shapes on top of each other.\n" \
-                              "  Layer 0: A square shape 4 blocks wide.\n" \
-                              "  Layer 1: Here we repeat the last layer, placing again a square shape 4 blocks wide.\n" \
-                              "  Layer 2: We widen the shape according to the user request."
-
-
-        self.system_prompt3 = "In order to build an L shaped house we must place 4 square shapes on the ground, there's no need for other layers. \n" \
-                              "  Layer 0: 4 blocks in a row from to form the long side of the L shape, with 2 blocks at the end in an angle to form the short side."
-
     async def get_llm_response(self, system_message, prompt, image_path="", debug=False):
 
 
