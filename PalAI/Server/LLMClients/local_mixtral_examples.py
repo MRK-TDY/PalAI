db1b409498802465dedb0fe3e2218199301d7c28
Manuel Guimarães <manuel.guimaraes@mrkite.io>
2024-04-04 19:38:13 +0100

    More examples

--- PalAI/Server/LLMClients/local_mixtral_examples.py
+++ PalAI/Server/LLMClients/local_mixtral_examples.py
@@ -8,7 +8,7 @@ def getArchitectExamples(prompt):
     example1_assistant = "In order to build a house that widens at the top we must place 3 square shapes on top of each other.\n \
   Layer 0: A square shape 4 blocks wide.\n \
   Layer 1: Here we repeat the last layer, placing again a square shape 4 blocks wide.\n \
-  Layer 2: We widen the shape according to the user request."
+  Layer 2: We widen the shape according to the user request.\n"
 
     messages.append({"role": "user", "content": example1_user})
     messages.append({"role": "assistant", "content": example1_assistant})
@@ -16,7 +16,7 @@ def getArchitectExamples(prompt):
     example2_user = "I want to build an L-shaped house."
 
     example2_assistant = "In order to build an L shaped house we must place 4 square shapes on the ground, there's no need for other layers.\n \
-  Layer 0: 4 blocks in a row from to form the long side of the L shape, with 2 blocks at the end in an angle to form the short side."
+  Layer 0: 4 blocks in a row from to form the long side of the L shape, with 2 blocks at the end in an angle to form the short side.\n"
 
     messages.append({"role": "user", "content": example2_user})
     messages.append({"role": "assistant", "content": example2_assistant})
@@ -26,7 +26,7 @@ def getArchitectExamples(prompt):
     example3_assistant = "In order to build a pyramid, we will create a structure that narrows as it goes up, resulting in a pointed top.\n \
   Layer 0: A square shape 5 blocks wide on each side to serve as the base. \n \
   Layer 1: A smaller square shape, 3 blocks wide, centered on top of the previous layer.\n \
-  Layer 2: A single block centered on top of the previous layer."
+  Layer 2: A single block centered on top of the previous layer.\n"
 
     messages.append({"role": "user", "content": example3_user})
     messages.append({"role": "assistant", "content": example3_assistant})
@@ -35,8 +35,8 @@ def getArchitectExamples(prompt):
 
     example4_assistant = "In order to build a tower with a balcony, we will create a tall structure with an extending section on one side.\n\
   Layer 0: A square shape 3 blocks wide on each side to form the base of the tower.\n\
-  Layer 1: Repeat the same square shape 3 blocks wide to continue the tower's structure.\\n\
-  Layer 2: Create a square shape 3 blocks wide for the tower, and extend a row of blocks out on one side to form the balcony."
+  Layer 1: Repeat the same square shape 3 blocks wide to continue the tower's structure.\n\
+  Layer 2: Create a square shape 3 blocks wide for the tower, and extend a row of blocks out on one side to form the balcony.\n"
 
     messages.append({"role": "user", "content": example4_user})
     messages.append({"role": "assistant", "content": example4_assistant})
@@ -60,7 +60,7 @@ def getBrickExamples(prompt):
   B:CUBE|3,0\n\
   Then the short side:\n\
   B:CUBE|0,1\n\
-  B:CUBE|0,2"
+  B:CUBE|0,2\n"
 
     messages.append({"role": "user", "content": example1_user})
     messages.append({"role": "assistant", "content": example1_assistant})
@@ -77,7 +77,7 @@ def getBrickExamples(prompt):
       B:CUBE|1,2\n\
       B:CUBE|2,2\n\
     Then the bridge between them:\n\
-      B:CUBE|1,1"
+      B:CUBE|1,1\n"
 
     messages.append({"role": "user", "content": example2_user})
     messages.append({"role": "assistant", "content": example2_assistant})
@@ -88,7 +88,7 @@ def getBrickExamples(prompt):
   B:CUBE|0,0\n\
   B:CUBE|1,0\n\
   B:CUBE|0,1\n\
-  B:CUBE|1,1"
+  B:CUBE|1,1\n"
 
     messages.append({"role": "user", "content": example3_user})
     messages.append({"role": "assistant", "content": example3_assistant})
@@ -136,17 +136,17 @@ Concrete Dark Stripes Blue, Marble, Dark Marble, Sand, Dark Red, Molten Marble, 
     example1_assistant = "Floor:Concrete White\n\
   Interior: Metal Blue\n\
   Exterior: Marble\n\
-  Style: modern"
+  Style: modern\n"
 
     messages.append({"role": "user", "content": example1_user})
     messages.append({"role": "assistant", "content": example1_assistant})
 
-    example2_user = "I want square block house.\n"+ system_message
+    example2_user = "I want square block house.\n" + system_message
 
     example2_assistant = "Floor:Concrete White\n\
       Interior: Metal Dark Blue\n\
       Exterior: Stone Light Grey\n\
-      Style: normal"
+      Style: normal\n"
 
     messages.append({"role": "user", "content": example2_user})
     messages.append({"role": "assistant", "content": example2_assistant})
@@ -156,11 +156,67 @@ Concrete Dark Stripes Blue, Marble, Dark Marble, Sand, Dark Red, Molten Marble, 
     example3_assistant = "Floor: Metal Blue\n\
          Interior: Marble\n\
          Exterior: Molten Marble\n\
-         Style: rounded"
+         Style: rounded\n"
 
     messages.append({"role": "user", "content": example3_user})
     messages.append({"role": "assistant", "content": example3_assistant})
 
-    messages.append({"role": "user", "content": prompt + system_message})
+    messages.append({"role": "user", "content": prompt})
+
+    return messages
+
+
+def getAddOnsExamples(prompt):
+
+    messages = []
+    example1_user = "Your job is to take the existing building and add doors and windows to it.\n \
+      Here is the syntax of the code that creates the buildings:\n  B:BLOCK_TYPE|LOCATION|LAYER|ADD-ON.\n" \
+                    "Create a block with no windows at point 0,0\n" \
+                    "B:CUBE|0,0|0"
+
+    example1_assistant = "B:CUBE|0,0|0"
+
+
+    messages.append({"role": "user", "content": example1_user})
+    messages.append({"role": "assistant", "content": example1_assistant})
+
+    example2_user = "Create a cube at the point 2,3, on the 1st layer (meaning above the other layer). " \
+                    "We then add a window to this block, facing the point 2,2.\n" \
+                    "Here is the building with no windows or doors\n" \
+                    "B:CUBE|0,0|0\n"\
+                    "B:CUBE|0,0|1\n" \
+                    "B:CUBE|0,0|2\n" \
+
+    example2_assistant = "B:CUBE|0,0|1|WINDOW|0,0"
+
+    messages.append({"role": "user", "content": example2_user})
+    messages.append({"role": "assistant", "content": example2_assistant})
+
+    example3_user = "Here is the building with no windows or doors\n" \
+                    "B:CUBE|0,0|0\n" \
+                    "B:CUBE|1,0|0\n" \
+                    "B:CUBE|0,1|0\n" \
+                    "B:CUBE|1,1|0\n" \
+                    "B:CUBE|1,1|1\n" \
+                    "B:CUBE|1,0|1\n"
+
+    example3_assistant = "B:CUBE|0,0|0|DOOR|0,0\n" \
+                         "B:CUBE|1,1|1|WINDOW|0,0"
+
+    messages.append({"role": "user", "content": example3_user})
+    messages.append({"role": "assistant", "content": example3_assistant})
+
+    example4_user = "Create a door in the middle of the house.\n" \
+                    "B:CUBE|0,0\n\
+                    B:CUBE|1,0\n\
+                    B:CUBE|0,1\n\
+                    B:CUBE|1,1"
+
+    example4_assistant = "B:CUBE|0,0|0|DOOR|0,0"
+
+    messages.append({"role": "user", "content": example4_user})
+    messages.append({"role": "assistant", "content": example4_assistant})
+
+    messages.append({"role": "user", "content": prompt})
 
     return messages
